ee7ca143d9bef4416ccefac8b648cc86683835d1
Iván Guerrero <ivangvprogrammer@gmail.com>
2023-12-05 21:57:23 -0500

    App

---
+++ index.js
@@ -0,0 +1,125 @@
+const express = require('express')
+const cors = require('cors')
+
+const app = express()
+
+app.use(cors())
+app.use(express.json())
+
+const requestLogger = (request, response, next) => {
+    console.log('Method:', request.method)
+    console.log('Path:  ', request.path)
+    console.log('Body:  ', request.body)
+    console.log('---')
+    next()
+}
+
+app.use(requestLogger)
+
+let notes = [
+    {
+        id: 1,
+        content: "HTML is easy",
+        date: "2019-05-30T17:30:31.098Z",
+        important: true
+    },
+    {    id: 2,
+        content: "Browser can execute only Javascript",
+        date: "2019-05-30T18:39:34.091Z",
+        important: false
+    },
+    {    id: 3,
+        content: "GET and POST are the most important methods of HTTP protocol",
+        date: "2019-05-30T19:20:14.298Z",
+        important: true
+    }
+]
+
+app.get('/', (request, response) => {
+    response.send('<h1>Hello World!</h1>')
+})
+
+app.get('/api/notes', (request, response) => {
+    response.json(notes)
+})
+
+app.get('/api/notes/:id', (request, response) => {
+    const id = Number(request.params.id)
+    console.log(id);
+    const note = notes.find(note => {
+        return note.id === id
+    })
+    if (note) {
+        response.json(note)
+    } else {
+        response.status(400).end()
+    }
+})
+
+app.delete('/api/notes/:id', (request, response) => {
+    const id = Number(request.params.id)
+    notes = notes.filter(note => note.id !== id)
+
+    response.status(204).end()
+})
+
+const generateId = () => {
+    const maxId = notes.length > 0
+    ? Math.max(...notes.map(n => n.id))
+    : 0
+
+    return maxId + 1
+}
+
+app.post('/api/notes', (request, response) => {
+    const note = request.body
+
+    if (!note.content) {
+        return response.status(400).json({
+            error: 'content missing'
+        })
+    }
+
+    const newNote = {
+        id: generateId(),
+        content: note.content,
+        important: typeof note.important !== 'undefined' ? note.important : false,
+        date: new Date()
+    }
+    console.log(newNote);
+
+    notes = [...notes, newNote]
+
+    response.json(newNote)
+})
+
+app.put('/api/notes/:id', (request, response) => {
+    const id = Number(request.params.id)
+    const data = request.body
+
+    const notaActualizada = notes.find(nota => nota.id === id)
+    if(!notaActualizada) {
+        return response.status(404).json({
+            error: 'Nota no encontrada'
+        })
+    }
+
+    const nota = {
+        content: notaActualizada.content = data.content,
+        important: notaActualizada.important = data.important
+    }
+
+    response.json(nota)
+})
+
+const unknownEndpoint = (request, response) => {
+    response.status(404).send({ error: 'unknown endpoint' })
+}
+
+app.use(unknownEndpoint)
+
+const PORT = 3001
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+})
+
